8afb25c481e70f3321fe20224057bf608713503d
AlanisMayumi <mayumi.alanis@gmail.com>
2022-11-06 16:32:02 -0300

    users

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.listen(5000, () => console.log('Backend server is running'))
+const users = [
+  {
+    id: 1,
+    userName: "Kim Nam Joon",
+    password: "knj2022",
+    isAdmin: true,
+  },
+  {
+    id: 2,
+    userName: "Min Yoon Gi",
+    password: "myg2022",
+    isAdmin: false,
+  },
+];
+
+app.post("/api/login", (req, res) => {
+  const { userName, password } = req.body;
+});
+
+app.listen(5000, () => console.log("Backend server is running"));

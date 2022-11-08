7dde83e4be7f548f796ece34c9b5d5f82a6a0cb6
AlanisMayumi <mayumi.alanis@gmail.com>
2022-11-08 00:15:09 -0300

    testing login endpoint

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const app = express();
+app.use(express.json());
 
 const users = [
   {
     id: 1,
-    userName: "Kim Nam Joon",
+    userName: "KimNamJoon",
     password: "knj2022",
     isAdmin: true,
   },
   {
     id: 2,
-    userName: "Min Yoon Gi",
+    userName: "MinYoonGi",
     password: "myg2022",
     isAdmin: false,
   },
   {
     id: 3,
-    userName: "Kim Seok Jin",
+    userName: "KimSeokJin",
     password: "ksj2022",
     isAdmin: false,
   },
@@ -25,6 +26,15 @@ const users = [
 
 app.post("/api/login", (req, res) => {
   const { userName, password } = req.body;
+
+  const user = users.find(
+    (x) => x.userName === userName && x.password === password
+  );
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400).json("User or password incorrect");
+  }
 });
 
 app.listen(5000, () => console.log("Backend server is running"));
